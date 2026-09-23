689c45fb4a9b9d9c665e38259760d5860eab7d74
Sumayyah Khalid <sumayyah.khaled@gmail.com>
2026-09-23 01:23:07 -0400

    Add OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,14 +30,14 @@ export const metadata: Metadata = {
     description,
     url: "https://sumkhalid.me",
     siteName: "Sumayyah Khalid",
-    images: [{ url: "/og.png", width: 1200, height: 630}],
+    images: [{ url: "/og.webp", width: 1200, height: 630}],
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title,
     description,
-    images: ["/og.png"],
+    images: ["/og.webp"],
   },
 };
 
